f2a7f826f52a1619a3db15d944d7fe1eadd26290
mohan-23 <mohankandukoori23@gmail.com>
2024-03-17 11:25:55 +0530

    third commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Photo from "../Assets/photo.png";
-import Resume from "../Assets/MohanCanva.pdf";
+import Resume from "../Assets/MohanCanva1.pdf";
 import { Styles } from "../Utils/Style";
 import { TiArrowDownOutline } from "react-icons/ti";
 import "../App.css";
